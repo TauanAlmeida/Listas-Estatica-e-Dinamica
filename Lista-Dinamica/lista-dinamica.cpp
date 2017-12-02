669082728756384751e4df1b2c10131fa94984d4
Tauan Almeida <tauan_almeida@hotmail.com>
2017-12-02 00:33:09 -0200

    Update #2

--- Lista-Dinamica/lista-dinamica.cpp
+++ Lista-Dinamica/lista-dinamica.cpp
@@ -1,16 +1,146 @@
 #include <stdio.h>
-#includle <stdlib.h>
+#include <stdlib.h>
+#include "lista-dinamica.h"
 
 int main(){
+	LISTA_ENC li;
+	cria_lista(&li);
+	eh_vazia (li);
+	ins (&li, 1, 1);
+	ins (&li, 2, 2);
+	ins (&li, 3, 3);
+	ins (&li, 4, 4);
+//	gera_lista(&li, 5, 10);
+//insere_ordenado(&li, 6);
+//insere_ordenado(&li, 3);
+//insere_ordenado(&li, 2);
+//insere_ordenado(&li, 4);
+
+	imprimi_lista(&li);
+	remove_com_base_no_valor(&li, 2);
+	imprimi_lista(&li);
+//pertence(&li, 4);
+//	eh_ordenada(&li);
+}
+
+void remove_com_base_no_valor(LISTA_ENC *pl, int valor){
+	NODO *aux;
+	NODO *ant;
+	int cont=0;
+	aux = (NODO *) malloc (sizeof(NODO));
+//	for (aux=*pl;aux->inf!=valor;aux=aux->next)ant=aux->next;
+	for (aux=*pl;aux->inf!=valor;aux=aux->next){
+		cont++;
+		ant=aux;
+	}
+	if (cont==0){
+		free()
+	}
+	ant->next = aux->next;
+//	return *pl;
+}
+
+
+LISTA_ENC insere_ordenado(LISTA_ENC *pl, int valor){
+	NODO *novo;
+	novo = (NODO *) malloc (sizeof(NODO));
+	if (*pl == NULL){
+		novo->next= NULL;
+		novo->inf=valor;
+		*pl=novo;
+		return *pl;
+	}else{
+		int aux2;
+		novo->inf=valor;
+		novo->next=NULL;
+		NODO *aux;
+		for (aux=*pl;aux->next!=NULL;aux=aux->next);
+		aux->next=novo;
+		for(aux=*pl;aux->next!=NULL;aux=aux->next){
+			if (novo->inf<aux->inf){
+			aux2 = aux->inf;
+			aux->inf = novo->inf;
+			novo->inf = aux2;
+			}
+		}
+		return *pl;
+	}
+}
+void gera_lista(LISTA_ENC* pl, int m, int n){
+NODO *novo;
+int k=1;
+	for (m;m<=n;m++){
+		novo = (NODO *) malloc (sizeof(NODO));
+	if (!novo) { printf ("\nERRO! Memoria insuficiente!\n");
+		exit (2); 
+	}
+	novo->inf = m;
+	if (k==1){
+		novo->next = *pl;
+		*pl = novo;
+	}else{
+		LISTA_ENC aux;
+	for (aux=*pl; k>2; aux=aux->next, k--);
+		novo->next = aux->next;
+		aux->next = novo;
+	}
+k++;
+	}
+}
+
+int eh_ordenada(LISTA_ENC *pl){
+	NODO* aux;
+	aux = (NODO *) malloc (sizeof(NODO));
+	aux = *pl;
+	while(aux->next){
+		if (aux->inf < aux->next->inf){
+		}else{
+			printf ("n ordenada!\n");
+		return 0;
+		}
+		aux = aux->next;
+	}
+	printf ("ordenada!\n");
+	return 1;
+}
+
+int pertence(LISTA_ENC *pl, int valor){
+	NODO* aux;
+	aux = (NODO *) malloc (sizeof(NODO));
+	aux = *pl;
+	while(aux->next){
+		if (aux->inf == valor){
+			printf ("\no numero %d pertence a lista!\n", aux->inf);
+			return 1;
+		}
+		aux = aux->next;	
+	}
+	printf("valor n encontrado!\n");
+	return 0;
+}
+void imprimi_lista(LISTA_ENC *pl){
+	NODO* aux = (NODO *) malloc (sizeof(NODO));
+
+		for(aux = *pl;aux;aux=aux->next){
+			printf ("%d \n",aux->inf);
+		}
+		printf ("\n");
+		
 	
 }
 
 void cria_lista (LISTA_ENC *pl){
 *pl=NULL;
+
 }
 
 int eh_vazia (LISTA_ENC l){
-return (l == NULL);
+	if (l == NULL){
+		printf ("Eh Vazia!\n");
+		return (l == NULL);
+	}else{
+		return 0;
+	}
 }
 
 int tam (LISTA_ENC l){
@@ -35,11 +165,13 @@ exit (2);
 novo->inf = v;
 if (k==1){
 novo->next = *pl;
+
 *pl = novo;
-}
-else {
+ 
+}else{
 LISTA_ENC aux;
 for (aux=*pl; k>2; aux=aux->next, k--);
+
 novo->next = aux->next;
 aux->next = novo;
 }
--- Lista-Dinamica/lista-dinamica.h
+++ Lista-Dinamica/lista-dinamica.h
@@ -4,6 +4,14 @@ int inf;
 struct nodo * next;
 }NODO;
 typedef NODO * LISTA_ENC;
+
+
+void remove_com_base_no_valor(LISTA_ENC *, int);
+LISTA_ENC insere_ordenado(LISTA_ENC*, int);
+void gera_lista(LISTA_ENC*, int , int );
+int eh_ordenada(LISTA_ENC *);
+int pertence(LISTA_ENC *, int);
+void imprimi_lista(LISTA_ENC *);
 void cria_lista (LISTA_ENC *);
 int eh_vazia (LISTA_ENC);
 int tam (LISTA_ENC);
